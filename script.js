7f3b0c27d80e0a083815a42b5abaa738ae0b219f
Daniel Ruiz González <danielbajamar2@gmail.com>
2025-02-16 11:01:13 +0000

    Redirección automática

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-const shortBtn = document.getElementById('short-btn');
-const reloadBtn = document.getElementById('reload-btn');
+const shortBtn = document.getElementById("short-btn");
+const reloadBtn = document.getElementById("reload-btn");
 
-shortBtn.addEventListener('click', shortenUrl);
+shortBtn.addEventListener("click", shortenUrl);
 
 function shortenUrl() {
-    var originalUrl = document.getElementById("originalUrl").value;
-    var apiUrl = "https://tinyurl.com/api-create.php?url=" + encodeURIComponent(originalUrl);
-    var shortenedUrlTextarea = document.getElementById("shortenedUrl");
+  var originalUrl = document.getElementById("originalUrl").value;
+  var apiUrl =
+    "https://tinyurl.com/api-create.php?url=" + encodeURIComponent(originalUrl);
+  var shortenedUrlTextarea = document.getElementById("shortenedUrl");
 
-    fetch(apiUrl).then(response => response.text()).then
-    (data => {
-        shortenedUrlTextarea.value = data;
-    }).catch(error => {
-        shortenedUrlTextarea.value = "Error: No se puede acortar la URL!!";
+  fetch(apiUrl)
+    .then((response) => response.text())
+    .then((data) => {
+      shortenedUrlTextarea.value = data + "?source=direct";
+    })
+    .catch((error) => {
+      shortenedUrlTextarea.value = "Error: No se puede acortar la URL!!";
     });
 }
 
-reloadBtn.addEventListener("click",() => {
-    location.reload();
+reloadBtn.addEventListener("click", () => {
+  location.reload();
 });
